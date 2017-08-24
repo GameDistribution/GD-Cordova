23dfa08c0f9158d28eb58c90dc77cc6771905eff
Emre Demir <yemre.demir@triodor.eu>
2017-08-24 11:18:16 +0300

    Adx integration added. Now working with real ads which served from adx

--- GDApi/www/GDApi.js
+++ GDApi/www/GDApi.js
@@ -4,14 +4,14 @@ exports.init = function(arg0, success, error) {
     exec(success, error, "GDApi", "init", arg0);
 };
 
-exports.showBanner = function(arg0, success, error) {
-    exec(success, error, "GDApi", "showBanner", arg0);
+exports.showBanner = function(success, error) {
+    exec(success, error, "GDApi", "showBanner", []);
 };
 
 exports.addTestDevice = function(arg0, success, error) {
     exec(success, error, "GDApi", "addTestDevice", arg0);
 };
 
-exports.setAdListener = function(arg0, success, error) {
-    exec(success, error, "GDApi", "setAdListener", arg0);
+exports.setAdListener = function(success, error) {
+    exec(success, error, "GDApi", "setAdListener", []);
 };
