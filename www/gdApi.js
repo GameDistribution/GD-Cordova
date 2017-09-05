bcf2f02f38cee571c8bd499dd82f5929da34663a
Emre Demir <yemre.demir@triodor.eu>
2017-09-05 16:49:22 +0300

    Some reference error fixed based on renaming things :P

--- www/gdApi.js
+++ www/gdApi.js
@@ -1,17 +1,17 @@
 var exec = require('cordova/exec');
 
 exports.init = function(arg0, success, error) {
-    exec(success, error, "GDApi", "init", arg0);
+    exec(success, error, "gdApi", "init", arg0);
 };
 
 exports.showBanner = function(success, error) {
-    exec(success, error, "GDApi", "showBanner", []);
+    exec(success, error, "gdApi", "showBanner", []);
 };
 
 exports.addTestDevice = function(arg0, success, error) {
-    exec(success, error, "GDApi", "addTestDevice", arg0);
+    exec(success, error, "gdApi", "addTestDevice", arg0);
 };
 
 exports.setAdListener = function(success, error) {
-    exec(success, error, "GDApi", "setAdListener", []);
+    exec(success, error, "gdApi", "setAdListener", []);
 };
